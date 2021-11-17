b60ba48ebefca8e5114ecd8a02271292bcc28533
justin <justin.d.diaz@gmail.com>
2021-11-16 19:10:41 -0500

    more css

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,59 +1,61 @@
 const Buttons = (props) => {
   return (
-    <div className="numbers">
-      <button onClick={props.clearHandler}>AC</button>
-      <button onClick={props.enterHandler} value="±">
+    <div className="buttons">
+      <button onClick={props.clearHandler} className="special">
+        AC
+      </button>
+      <button onClick={props.enterHandler} value="±" className="special">
         ±
       </button>
-      <button onClick={props.enterHandler} value="%">
+      <button onClick={props.enterHandler} value="%" className="special">
         %
       </button>
-      <button onClick={props.enterHandler} value="1">
+      <button onClick={props.enterHandler} value="1" className="numbers">
         1
       </button>
-      <button onClick={props.enterHandler} value="2">
+      <button onClick={props.enterHandler} value="2" className="numbers">
         2
       </button>
-      <button onClick={props.enterHandler} value="3">
+      <button onClick={props.enterHandler} value="3" className="numbers">
         3
       </button>
-      <button onClick={props.enterHandler} value="4">
+      <button onClick={props.enterHandler} value="4" className="numbers">
         4
       </button>
-      <button onClick={props.enterHandler} value="5">
+      <button onClick={props.enterHandler} value="5" className="numbers">
         5
       </button>
-      <button onClick={props.enterHandler} value="6">
+      <button onClick={props.enterHandler} value="6" className="numbers">
         6
       </button>
-      <button onClick={props.enterHandler} value="7">
+      <button onClick={props.enterHandler} value="7" className="numbers">
         7
       </button>
-      <button onClick={props.enterHandler} value="8">
+      <button onClick={props.enterHandler} value="8" className="numbers">
         8
       </button>
-      <button onClick={props.enterHandler} value="9">
+      <button onClick={props.enterHandler} value="9" className="numbers">
         9
       </button>
-      <button onClick={props.enterHandler} value="0">
+      <button onClick={props.enterHandler} value="0" className="numbers">
         0
       </button>
       <button onClick={props.enterHandler} value=".">
         .
       </button>
-      <button onClick={props.enterHandler} value="÷">
+      <button onClick={props.enterHandler} value="÷" className="character">
         ÷
       </button>
-      <button onClick={props.enterHandler} value="×">
+      <button onClick={props.enterHandler} value="×" className="character">
         ×
       </button>
-      <button onClick={props.enterHandler} value="-">
+      <button onClick={props.enterHandler} value="-" className="character">
         -
       </button>
-      <button onClick={props.enterHandler} value="+">
+      <button onClick={props.enterHandler} value="+" className="character">
         +
       </button>
-      <button onClick={props.equalHandler} value="=">
+      <button onClick={props.equalHandler} value="=" className="character">
         =
       </button>
     </div>
